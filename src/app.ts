5b70a1582bb48bef9bbae625bc226e888cd27975
Kirti Dodekar <kirtidodekar05@gmail.com>
2026-02-08 19:56:23 +0530

    Fix Genkit dependency versions for Vercel build

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,8 @@ const allowedOrigins = [
     'https://agricycle-connect.vercel.app',
     'http://localhost:3000',
     'http://localhost:5173',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'http://localhost:8080'
 ];
 
 app.use(cors({
